41807bd20d652b6e5eac138f7b508293763a7993
Bladimir Miranda Luna <jbmirandal@hotmail.com>
2024-06-07 13:01:14 -0500

    Agregar archivos de proyecto.

---
+++ CamaraWasm/wwwroot/js/camara.js
@@ -0,0 +1,53 @@
+﻿export async function inicializarcamara() {
+    //const constraints = {
+    //    video: true
+    //};
+
+    //const video = document.getElementById('video');
+    //const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    //video.srcObject = stream;
+
+    const constraints = {
+        video: true
+    };
+
+    try {
+        const video = document.getElementById('video');
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        video.srcObject = stream;
+    } catch (error) {
+        console.error('Error al acceder a la cámara: ', error);
+        throw error;
+    }
+}
+
+export function tomarFoto() {
+    //const video = document.getElementById('video');
+    //const canvas = document.getElementById('canvas');
+    //const context = canvas.getContext('2d');
+
+    //canvas.width = video.videoWidth;
+    //canvas.height = video.videoHeight;
+
+    //context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    //const dataUrl = canvas.toDataURL('image/png');
+    //return dataUrl; // Devuelve la URL de la imagen en formato base64
+    try {
+        const video = document.getElementById('video');
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
+
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        const dataUrl = canvas.toDataURL('image/png');
+        return dataUrl; // Devuelve la URL de la imagen en formato base64
+    } catch (error) {
+        console.error('Error al tomar la foto: ', error);
+        throw error;
+    }
+}
+
